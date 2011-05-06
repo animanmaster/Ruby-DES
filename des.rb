3b868e47408b683ad1251d28dd6cef4a2a19d433
cirner <cirner@franklin.hopto.org>
2011-05-05 23:49:24 -0400

    Added DESCBCAttack methods and well as the Message Class

--- des.rb
+++ des.rb
@@ -199,6 +199,27 @@ class CBC
     end
 end
 
+class Message
+    def to_ascii(binary_str)
+        binary_str.gsub(/\s/,'').gsub(/([01]{8})/) { |b| b.to_i(2).chr }
+    end
+
+    def self.checkValidity(message)
+        message = to_ascii(message)
+        counter = 0;
+        file = File.new("/home/remococco/code/java/HillCipher/src/hillcipher/bruteforce/dictionaries/english-words.all", "r")
+        while (line = file.gets)
+            if(message.include? line)
+                counter += 1
+            end
+        end
+        file.close
+        if(counter >= 10)
+            puts message
+        end
+    end
+end
+
 class DESCBCAttack
     attr_accessor :key, :iv, :cipherText
     
@@ -213,6 +234,7 @@ class DESCBCAttack
             des = DES.new(@key)
             begin
                 cbc = CBC.new(des, @iv, @cipherText).decipher
+                Message.checkValidity(cbc)
             end until @iv.exhaustedIncreases()
         end until @key.exhaustedIncreases()
     end
